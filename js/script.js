dbe60ab60b0548a9636c163dd6340ff4e11a95a7
Ilya Bondar <iliyabondar26@gmail.com>
2020-03-31 01:24:56 +0300

    Мобильная версия сайта готоваgit add .!

--- js/script.js
+++ js/script.js
@@ -2,23 +2,30 @@
 let data_index_elements = document.querySelectorAll('[data_index]');
 let page_wrapper = document.querySelector('.page_wrapper');
 let pages = document.querySelectorAll('.page');
-let page_number = pages.length;
 let contacts = document.querySelector('.contacts');
+let button_burger = document.querySelector('.burger_button');
+let navbar = document.querySelector('.navbar');
+let float_right = navbar.querySelector('.float_right');
+let navbar_items = float_right.querySelectorAll('a');
+let page_number = pages.length;
 let active_page = 1;
 let scroll_status = 1;
 let device_scrolling = 'desktop';
 let scrolling = 1;
 let first_resize = 1;
+
 function contacts_transition() {
     contacts.querySelector('div').style.transition = 'opacity, 1000ms';
     page_wrapper.style.transition = 'blur, 1000ms';
 }
-document.querySelector('.contacts_button').onclick = () => {
+
+document.querySelector('.contacts_button').addEventListener('click', () => {
     contacts_transition();
     contacts.style.visibility = 'visible';
     page_wrapper.style.filter = 'blur(8px)';
     contacts.querySelector('div').style.opacity = '1';
-}
+});
+
 contacts.querySelector('.button_close').onclick = () => {
     contacts_transition();
     contacts.style.visibility = 'hidden';
@@ -60,28 +67,56 @@ function scroll(call) {
 }
 for (let i = 0; i < data_index_elements.length; i++) {
     let data_index_element = data_index_elements[i];
-    data_index_element.onclick = (e) => {
+    data_index_element.addEventListener('click', () => {
         scroll(() => {
             active_page = +data_index_element.getAttribute('data_index');
         });
-    }
+    });
 }
 
+
 window.addEventListener('resize', () => {
+    if (getComputedStyle(button_burger).display == 'block') {
+        button_burger.onclick = () => {
+            if (button_burger.classList.contains('fa-bars')) {
+                navbar.style.height = '100%';
+                float_right.style.opacity = '1';
+                button_burger.classList.remove('fa-bars');
+                button_burger.classList.add('fa-times');
+                button_burger.style.color = 'white';
+            } else {
+                navbar.style.height = '0';
+                float_right.style.opacity = '0';
+                button_burger.classList.remove('fa-times');
+                button_burger.classList.add('fa-bars');
+                button_burger.style.color = '';
+            }
+        }
+
+        for (let i = 0; i < navbar_items.length; i++) {
+            let navbar_item = navbar_items[i];
+            navbar_item.onclick = () => {
+                button_burger.dispatchEvent(new Event('click'));
+            }
+        }
+    } else {
+        navbar.style = '';
+        float_right.style = '';
+    }
     if (window.matchMedia('(max-width: 850px)').matches) {
         window.onscroll = (e) => {
             scroll(() => {
                 scrolling = 0;
                 if (device_scrolling == 'laptop' || first_resize) {
                     while (true) {
-                        let nextPageScrollTop = 1;
+                        let nextPageScrollTop = 34;
                         if (pages[active_page] !== undefined) {
                             nextPageScrollTop = pages[active_page].getBoundingClientRect().y;
                         }
                         let prevPageScrollTop = pages[active_page - 1].getBoundingClientRect().y;
-                        if (nextPageScrollTop < 1) {
+                        if (nextPageScrollTop < 34) {
                             active_page++;
-                        } else if (prevPageScrollTop > 1) {
+                        } else if (prevPageScrollTop > 34) {
                             active_page--;
                         } else {
                             break;
